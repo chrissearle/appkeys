85aa29845538593bd9178fa583a6d5b7a44aee15
Chris Searle <chris@chrissearle.org>
2011-12-10 15:48:47 +0100

    Partials and add heroku config output

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def heroku(envkeys)
+    pairs = envkeys.map do |envkey|
+      "#{envkey.name}=#{envkey.value}"
+    end
+
+    "heroku config:add #{pairs.join ' '}".html_safe
+  end
 end
